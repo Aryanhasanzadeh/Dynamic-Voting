9dd5bdb7f8da4f52cd12608f18f8e9e2eb2a258d
Aryan Hasanzadeh <aryanhasanzadeh75@gmail.com>
2022-09-01 11:41:12 +0430

    config composer and factory

---
+++ database/factories/VoteTitleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Aryanhasanzadeh\Voteing\Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Aryanhasanzadeh\Voteing\App\Models\VoteTitle;
+
+class TranslateFactory extends Factory
+{
+
+    protected $model= VoteTitle::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(20),
+            'status' => rand(0,1),
+        ];
+    }
+
+}
